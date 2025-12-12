c9d2b4fa0f0834897bdf844deb905ce19344795a
Abhay Yadav <abhayleoyadav188102@gmail.com>
2025-12-13 00:14:37 +0530

    Initial commit

---
+++ CH-1/hello1.py
@@ -0,0 +1,11 @@
+#  CHAPTER 1 - MODULES, COMMENTS AND PIP
+
+#  python discoverd by [Guido van rossum] in 1991.
+# In this chapter we learn Modules,comments and pip. 
+#  FOR opening your vs code in any folder go to that folder and right click on blank space see there is vs code click boom you under your folder. (for wrap your paras i mean alt + z)
+# vs code is editor efficient editor is help full for write codes you write our code on notepad also 
+#  our extention of our python file is .py just like i name my file is hello1.py 
+# our first code is here -------------->
+
+
+print("Hello world")
---
+++ ch-0/hello.py
@@ -0,0 +1,11 @@
+# ULTIMATE COURSE OF PYTHON - CWH (CODE WITH HARRY)
+# CHAPTER - 0 -> THE BEGINNING 
+
+# 1> Install python from internet or chrome.
+    # * ensure that when you installing python tick the Add python.exe to path 
+# 2> Then download VS code IDE for write our codes.
+#   * mouse wheel search on search bar of VS code and tick the mouse wheel zoom it is help full acc to Harry.
+#   * And then download extention of python in the section of you see left bar second last icon.
+#   chapter END here see you in next chapter ----------------------------------------------------------------------------  
+
+#  FROM MY SIDE IN TOP BAR FILE OPEN IT AND TICK THE AUTO SAVE .
